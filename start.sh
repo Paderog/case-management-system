a14aeeba93ee233e96d03c0bb4f70354354c525c
Paderog <2201102223@student.buksu.edu.ph>
2026-03-31 14:46:55 +0800

    Add startup logs for migrate and seed

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
+set -e
 
+echo "=== APP STARTING ==="
+php artisan optimize:clear || true
+
+echo "=== RUNNING MIGRATIONS ==="
 php artisan migrate --force
+
+echo "=== RUNNING SEEDER ==="
 php artisan db:seed --force
+
+echo "=== STARTING APACHE ==="
 apache2-foreground
